b7533c2ab40af22f03553e60b3c0a4036ec0fb12
LuciusCouto <97913684+LuciusCouto@users.noreply.github.com>
2022-04-08 14:26:19 -0300

    .

--- scroll.js
+++ scroll.js
@@ -1,37 +1,17 @@
-const menuItems = document.querySelectorAll('.nav-list a[href^="#"]');
+const link = document.querySelectorAll('html a[href^="#"]');
 
-menuItems.forEach(item => {
+link.forEach(item => {
     item.addEventListener('click', scrollToIdOnClick);
 })
 
 function getScrollTopByHref(element) {
     const id = element.getAttribute('href');
-    return document.querySelector(id).offsetTop;
-}
 
-function scrollToIdOnClick(event) {
-    event.preventDefault();
-    const to = getScrollTopByHref(event.target);
-
-    scrollToPosition(to);
-}
-
-function scrollToPosition(to) {
-    window.scroll({
-        top: to,
-        behavior: "smooth"
-    })
-}
-
-const menuItems2 = document.querySelectorAll('.nav-list2 a[href^="#"]');
-
-menuItems2.forEach(item => {
-    item.addEventListener('click', scrollToIdOnClick);
-})
-
-function getScrollTopByHref(element) {
-    const id = element.getAttribute('href');
-    return document.querySelector(id).offsetTop;
+    if (id == null) {
+        return document.querySelector(id);
+    } else {
+        return document.querySelector(id).offsetTop;
+    }   
 }
 
 function scrollToIdOnClick(event) {
