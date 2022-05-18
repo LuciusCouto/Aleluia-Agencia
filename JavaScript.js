c3a172725b3af4a056e7e1f8eadc917ad057fec7
LuciusCouto <97913684+LuciusCouto@users.noreply.github.com>
2022-05-18 12:51:51 -0300

    .

--- JavaScript.js
+++ JavaScript.js
@@ -1,11 +1,11 @@
 function iconemodifica() {
     var iconeAleluia = window.document.querySelector(".marca_aleluia_agencia");
 
-    if (clientWidth < 400) {
+    if (window.innerWidth < 400) {
         iconeAleluia.src = "imagens/Icones, marcas e logotipos/ìcone-do-site.png";
         iconeAleluia.style.width = "30px";
         iconeAleluia.style.height = "30px";
-    } else if (clientWidth > 400 && clientWidth < 1300) {
+    } else if (window.innerWidth > 400 && window.innerWidth < 1300) {
         iconeAleluia.src = "imagens/Icones, marcas e logotipos/Logotipo.png";
         iconeAleluia.style.width = "89.8px";
         iconeAleluia.style.height = "29.9px";
